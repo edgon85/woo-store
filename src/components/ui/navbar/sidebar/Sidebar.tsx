3381d41d7b443ac9442781581ec44ed1791b294e
Edwin Gonzalez <edgon85@gmail.com>
2024-10-14 17:25:02 -0600

    modificando sidebar para mejor scroll

--- src/components/ui/navbar/sidebar/Sidebar.tsx
+++ src/components/ui/navbar/sidebar/Sidebar.tsx
@@ -9,12 +9,24 @@ import { ArrowRightIcon, ListGroup, SidebarHelp } from '@/components';
 
 import clsx from 'clsx';
 import { useAuthStore } from '@/stores/auth.store';
+import { useEffect } from 'react';
 
 const Sidebar = () => {
   const menuOpen = useSidebar((state) => state.sidebarOpen);
   const setMenuOpen = useSidebar((state) => state.onSidebarOpen);
-  // const { user } = useAuth();
   const user = useAuthStore((state) => state.user);
+
+  useEffect(() => {
+    if (menuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset';
+    }
+
+    return () => {
+      document.body.style.overflow = 'unset';
+    };
+  }, [menuOpen]);
 
   return (
     <>
@@ -32,52 +44,65 @@ const Sidebar = () => {
       {/* Sidebar */}
       <div
         className={clsx(
-          'fixed top-0 right-0 h-screen w-3/4 bg-white transition-all duration-300 ease-in-out  md:hidden z-50 overflow-y-scroll',
+          'fixed top-0 right-0 h-screen w-3/4 bg-white transition-all duration-300 ease-in-out md:hidden z-50 overflow-y-auto',
           {
             'translate-x-full': !menuOpen,
           }
         )}
       >
-        <ul className="border-b shadow-sm h-16 w-full flex justify-start items-center pl-2">
-          <li>
-            <button onClick={setMenuOpen} className="w-8 h-8" aria-label="menu">
-              <ArrowRightIcon />
-            </button>
-          </li>
-        </ul>
-        <ul className="mt-8 pb-8 mx-4 space-y-2">
-          {user && (
-            <li className="mt-2">
-              <ButtonCellNow />
+        <div className="flex flex-col h-full">
+          <ul className="border-b shadow-sm h-16 w-full flex justify-start items-center pl-2 flex-shrink-0">
+            <li>
+              <button
+                onClick={setMenuOpen}
+                className="w-8 h-8"
+                aria-label="menu"
+              >
+                <ArrowRightIcon />
+              </button>
             </li>
-          )}
-          {!user && (
-            <li className="mt-2">
-              <ButtonRegister />
-            </li>
-          )}
+          </ul>
+          <div className="flex-grow overflow-y-auto">
+            <ul className="mt-8 pb-8 mx-4 space-y-2">
+              {user && (
+                <li className="mt-2">
+                  <ButtonCellNow />
+                </li>
+              )}
+              {!user && (
+                <li className="mt-2">
+                  <ButtonRegister />
+                </li>
+              )}
 
-          <li className="text-center text-lg font-semibold">Categorías</li>
+              <li className="text-center text-lg font-semibold">Categorías</li>
 
-          <MenuItem title={'Ropa'} items={<ClothesList articleType="ropa" />} />
-          <MenuItem
-            title={'Zapatos'}
-            items={<ClothesList articleType="zapatos" />}
-          />
-          <MenuItem
-            title={'Accesorios'}
-            items={<ClothesList articleType="accesorios" />}
-          />
-        </ul>
-        {user && (
-          <div>
-            <p className="text-center text-lg font-semibold">Ajustes</p>
+              <MenuItem
+                title={'Ropa'}
+                items={<ClothesList articleType="ropa" />}
+              />
+              <MenuItem
+                title={'Zapatos'}
+                items={<ClothesList articleType="zapatos" />}
+              />
+              <MenuItem
+                title={'Accesorios'}
+                items={<ClothesList articleType="accesorios" />}
+              />
+            </ul>
+            {user && (
+              <div>
+                <p className="text-center text-lg font-semibold">Ajustes</p>
 
-            <ListGroup userId={user?.id!} isMobile={true} />
+                <ListGroup userId={user?.id!} isMobile={true} />
+              </div>
+            )}
+
+            <SidebarHelp isMobile={true} />
+
+            <div className='h-20'></div>
           </div>
-        )}
-
-        <SidebarHelp isMobile={true} />
+        </div>
       </div>
     </>
   );
