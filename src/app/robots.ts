31acda600249020c14df5dfe61dbef870ac0f6e4
Edwin Gonzalez <edgon85@gmail.com>
2024-10-22 11:01:24 -0600

    cambio en robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,23 +2,20 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: [
-        '/private/',
-        '/admin/',
-        '/secret/',
-        '/product/create/',
-        '/product/edit/',
-        '/inbox/',
-        '/checkout/',
-        '/admin/',
-        '/settings/',
-        '/claim/',
-        '/api/admin/',
-      ],
-    },
-    sitemap: 'https://acme.com/sitemap.xml',
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: 'facebookexternalhit',
+        allow: '/',
+      },
+      {
+        userAgent: 'googlebot',
+        allow: '/',
+      },
+    ],
+    sitemap: 'https://www.woo.com.gt/sitemap.xml',
   };
 }
